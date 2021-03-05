023e90d27395d727bf28a7c428b30b2e133a52c7
NJL7502L <g.hana1211.tako@gmail.com>
2021-03-05 23:26:19 +0900

    0-99まで100msごとにインクリメントしてシリアル出力するプログラム

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,9 @@
 #include <M5Core2.h>
 
 void setup() { Serial.begin(115200); }
-void loop() { Serial.println("Hello world."); }
+void loop() {
+  for (int i = 0; i < 100; i++) {
+    Serial.println(i);
+    delay(100);
+  }
+}
